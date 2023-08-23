63fd73d6427ac7de49e2b1fd7311e1dbc864e5ff
gunurung <gw71211@gmail.com>
2023-08-23 22:20:11 +0900

    1

--- com/gnurung/music/Music.py
+++ com/gnurung/music/Music.py
@@ -3,15 +3,12 @@ import struct
 import winsound
 import numpy as np
 
-
 class Music:
     def __init__(self, channels, sampleRate, sampleWidth, data, normalized=False):
         self.channels = channels
         self.sampleRate = sampleRate
-        self.sampleWidth = sampleWidth
-        self.data: np.ndarray = data if normalized else self.normalize(data)
-        print(len(data))
-        print(len(self.data))
+        self.sampleWidth = sampleWidth  # byte 몇개가 필요한지
+        self.amps: np.ndarray = data if normalized else self.normalize(data)
 
     @classmethod
     def from_file(cls, wavfile):
@@ -21,62 +18,50 @@ class Music:
 
     @classmethod
     def copy(cls, music):
-        return Music(music.channels, music.sampleRate, music.sampleWidth, music.data, normalized=True)
+        return Music(music.channels, music.sampleRate, music.sampleWidth, music.amps.copy(), normalized=True)
 
     def play(self):
         winsound.PlaySound(self.towav(), winsound.SND_MEMORY)
 
     def normalize(self, data: bytes) -> np.ndarray:
-        normalized = np.frombuffer(data, dtype=float)
-
-        map(lambda x: x / self.sampleWidth, normalized)
+        size = int(len(data) / self.sampleWidth)
+        max_height = 2 ** (self.sampleWidth * 8) / 2
+        normalized = np.empty(size)
+        for i in range(0, size):
+            sample = int.from_bytes(data[self.sampleWidth * i: self.sampleWidth * (i+1)], "little", signed=True)
+            normalized[i] = sample / max_height
+        print(normalized)
         return normalized
 
-    def denormalize(self) -> bytes:
-        denorm = np.copy(self.data)
-        map(lambda x: x * self.sampleWidth, denorm)
-        return denorm.tobytes()
+    def denormalize(self) -> bytearray:
+        denorm = bytearray(len(self.amps) * self.sampleWidth)
+        max_height = int(2 ** (self.sampleWidth * 8) / 2)
+        for i in range(0, len(self.amps)):
+            sample = int(self.amps[i] * (2 ** (self.sampleWidth * 8)))
+            if sample < -max_height:
+                sample = -max_height
+            elif sample >= max_height:
+                sample = max_height-1
+
+            samples = sample.to_bytes(self.sampleWidth, "little", signed=True)
+
+            for j in range(0, len(samples)):
+                denorm[self.sampleWidth * i + j] = samples[j]
+        return denorm
 
     def towav(self):
-        wav = [struct.pack('<4s', b'RIFF'), struct.pack('I', len(self.data) + 36), struct.pack('4s', b'WAVE'),
+        wav = [struct.pack('<4s', b'RIFF'), struct.pack('I', len(self.amps) * self.sampleWidth + 36), struct.pack('4s', b'WAVE'),
                struct.pack('4s', b'fmt '), struct.pack('IHHIIHH', 16, 1, self.channels,
                                                        self.sampleRate,
                                                        self.sampleWidth * self.sampleRate * self.channels,
                                                        self.sampleWidth * self.channels,
                                                        self.sampleWidth * 8),
-               struct.pack('<4s', b'data'), struct.pack('I', len(self.data)), self.denormalize()]
+               struct.pack('<4s', b'data'), struct.pack('I', len(self.amps) * self.sampleWidth), self.denormalize()]
         return b''.join(wav)
 
     def inverse_phase(self):
-        denormalized = self.denormalize()
-        data = bytearray(len(denormalized))
-        for i in range(0, int(len(denormalized) / 2)):
-            sample = int.from_bytes([denormalized[2 * i], denormalized[2 * i + 1]], "little", signed=True) * -1
-            if sample < -32768:
-                sample = 32768
-            elif sample > 32767:
-                sample = 32767
-            samples = sample.to_bytes(2, "little", signed=True)
-            data[2 * i] = samples[0]
-            data[2 * i + 1] = samples[1]
-        self.data = self.normalize(data)
+        for i in range(0, len(self.amps)):
+            self.amps[i] *= -1
 
-    def integrate_pcm(self, lay):
-        denormalized = self.denormalize()
-        data = bytearray(len(denormalized))
-        for i in range(0, int(len(denormalized) / 2)):
-            sample1 = int.from_bytes([denormalized[2 * i], denormalized[2 * i + 1]], "little", signed=True)
-            if sample1 < -32768:
-                sample1 = 32768
-            elif sample1 > 32767:
-                sample1 = 32767
-            sample2 = int.from_bytes([lay[2 * i], lay[2 * i + 1]], "little", signed=True)
-            if sample2 < -32768:
-                sample2 = 32768
-            elif sample2 > 32767:
-                sample2 = 32767
-
-            samples = (sample1 + sample2).to_bytes(2, "little", signed=True)
-            data[2 * i] = samples[0]
-            data[2 * i + 1] = samples[1]
-        self.data = self.normalize(data)
+    def integrate_pcm(self, music):
+        self.amps = self.amps + music.amps
--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import tensorflow as tf
 import keras
 import asyncio
 from com.gnurung.music.Music import Music
-
 print(tf.version)
 music = Music.from_file("C:\\Users\\gw712\\Desktop\\Project-V\\input.wav")
 music_inv = Music.copy(music)
 music_inv.inverse_phase()
 
 print("play")
-music_inv.play()
-music.sampleWidth = 8
+music_inv.integrate_pcm(music)
 music.play()
